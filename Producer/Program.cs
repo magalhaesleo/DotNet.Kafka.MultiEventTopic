4d08187c6e085dea78a0f2040797b03559fff484
magalhaesleo <leonardomagalhaes.leo@gmail.com>
2026-06-04 18:14:04 -0300

    Replace single-event schema integration with multi-event topic support

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,13 +1,33 @@
-﻿using Confluent.Kafka;
+﻿using Avro.Specific;
+using Confluent.Kafka;
 using Confluent.SchemaRegistry;
 using Confluent.SchemaRegistry.Serdes;
-using Contracts;
+using DotNet.Kafka.MultiEventTopic;
 
 namespace Producer;
 
-class Program
+public static class Program
 {
-    static async Task Main(string[] args)
+    private static IEnumerable<ISpecificRecord> Events()
+    {
+        yield return new AccountOpened
+        {
+            eventId = Guid.NewGuid().ToString(),
+            accountId = Guid.NewGuid().ToString(),
+            ownerName = "",
+            openedAt = DateTime.UtcNow
+        };
+        yield return new MoneyDeposited
+        {
+            eventId = Guid.NewGuid().ToString(),
+            accountId = Guid.NewGuid().ToString(),
+            amount = 100,
+            occurredAt = DateTime.UtcNow,
+            transactionId = Guid.NewGuid().ToString()
+        };
+    }
+    
+    public static async Task Main(string[] args)
     {
         var config = new ProducerConfig { BootstrapServers = "localhost:9092", LingerMs = 0 };
         
@@ -16,25 +36,29 @@ class Program
             Url = "localhost:8081"
         };
 
+        var valueSerializerConfiguration = new AvroSerializerConfig
+        {
+            SubjectNameStrategy = SubjectNameStrategy.TopicRecord,
+            AutoRegisterSchemas = true
+        };
+
         using var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
-        using var producer = new ProducerBuilder<Null, AccountOpened>(config)
-            .SetValueSerializer(new AvroSerializer<AccountOpened>(schemaRegistry))
+        using var producer = new ProducerBuilder<Null, ISpecificRecord>(config)
+            .SetValueSerializer(new AvroSerializer<ISpecificRecord>(schemaRegistry, valueSerializerConfiguration))
             .Build();
 
         try
         {
-            var accountOpened = new AccountOpened();
-            var message = new Message<Null, AccountOpened> { Value = accountOpened };
-            var dr = await producer.ProduceAsync("account-events", message);
-            Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+            foreach (var @event in Events())
+            {
+                var message = new Message<Null, ISpecificRecord> { Value = @event };
+                var dr = await producer.ProduceAsync("account-events", message);
+                Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+            }
         }
         catch (ProduceException<Null, string> e)
         {
             Console.WriteLine($"Delivery failed: {e.Error.Reason}");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
         }
     }
 }
