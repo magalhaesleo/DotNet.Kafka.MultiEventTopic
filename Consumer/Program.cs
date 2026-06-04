14d26d15ffc3e700a9e5d40a7868095d4ff66dd9
magalhaesleo <leonardomagalhaes.leo@gmail.com>
2026-06-04 17:12:20 -0300

    Initialize Kafka producer and consumer projects with Avro-serialized schemas

---
+++ Consumer/Program.cs
@@ -0,0 +1,11 @@
+﻿using Confluent.Kafka;
+
+namespace Consumer;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,40 @@
+﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Contracts;
+
+namespace Producer;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var config = new ProducerConfig { BootstrapServers = "localhost:9092", LingerMs = 0 };
+        
+        var schemaRegistryConfig = new SchemaRegistryConfig
+        {
+            Url = "localhost:8081"
+        };
+
+        using var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
+        using var producer = new ProducerBuilder<Null, AccountOpened>(config)
+            .SetValueSerializer(new AvroSerializer<AccountOpened>(schemaRegistry))
+            .Build();
+
+        try
+        {
+            var accountOpened = new AccountOpened();
+            var message = new Message<Null, AccountOpened> { Value = accountOpened };
+            var dr = await producer.ProduceAsync("account-events", message);
+            Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+        }
+        catch (ProduceException<Null, string> e)
+        {
+            Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
