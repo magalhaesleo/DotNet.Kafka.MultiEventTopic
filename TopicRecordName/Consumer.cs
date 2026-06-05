f6acdff5aa5de60b44d61990979770348ffaabf0
magalhaesleo <leonardomagalhaes.leo@gmail.com>
2026-06-05 14:21:47 -0300

    Add support for topic-record name strategy with producer and consumer implementation

---
+++ TopicRecordName/Consumer.cs
@@ -0,0 +1,69 @@
+﻿using Avro.Specific;
+using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using DotNet.Kafka.MultiEventTopic;
+using YCherkes.SchemaRegistry.Serdes.Avro;
+
+namespace TopicRecordName;
+
+public static class Consumer
+{
+    public static void Consume(
+        string topicName,
+        string bootstrapServers,
+        string schemaRegistryUrl,
+        CancellationToken cancellationToken)
+    {  
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = bootstrapServers,
+            GroupId = Guid.NewGuid().ToString(),
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+        
+        var avroDeserializerConfig = new AvroDeserializerConfig()
+        {
+            SubjectNameStrategy = SubjectNameStrategy.TopicRecord
+        };
+        var schemaRegistryConfig = new SchemaRegistryConfig
+        {
+            Url = schemaRegistryUrl,
+        };
+        using var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
+        using var consumer = new ConsumerBuilder<string, ISpecificRecord>(config)
+            .SetValueDeserializer(new MultiSchemaAvroDeserializer(schemaRegistry, avroDeserializerConfig).AsSyncOverAsync())
+            .Build();
+        
+        consumer.Subscribe(topicName);
+        
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                var consumeResult = consumer.Consume(cancellationToken);
+                if (consumeResult.IsPartitionEOF)
+                    continue;
+                
+                var record = consumeResult.Message.Value;
+                switch (record)
+                {
+                    case AccountOpened accountOpened:
+                        Console.WriteLine(
+                            $"AccountOpened: {accountOpened.accountId} Owner: {accountOpened.ownerName} ");
+                        break;
+                    case MoneyDeposited moneyDeposited:
+                        Console.WriteLine($"MoneyDeposited: {moneyDeposited.amount} to {moneyDeposited.accountId}");
+                        break;
+                    default:
+                        Console.WriteLine("Unknown record: " + record?.GetType());
+                        break;
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+        }
+    }
+}
---
+++ TopicRecordName/Messages.cs
@@ -0,0 +1,38 @@
+﻿using Avro.Specific;
+using Confluent.Kafka;
+using DotNet.Kafka.MultiEventTopic;
+
+namespace TopicRecordName;
+
+public static class Messages
+{
+    public static IEnumerable<Message<string, ISpecificRecord>> Events()
+    {
+        var accountId = Guid.NewGuid().ToString();
+
+        yield return new Message<string, ISpecificRecord>()
+        {
+            Key = accountId,
+            Value = new AccountOpened()
+            {
+                accountId = accountId,
+                eventId = Guid.NewGuid().ToString(),
+                ownerName = "leonardo",
+                openedAt = DateTime.UtcNow,
+            }
+        };
+        
+        yield return new Message<string, ISpecificRecord>()
+        {
+            Key = accountId,
+            Value = new MoneyDeposited
+            {
+                accountId = accountId,
+                amount = 100,
+                eventId = Guid.NewGuid().ToString(),
+                occurredAt = DateTime.UtcNow,
+                transactionId = Guid.NewGuid().ToString(),
+            }
+        };
+    }
+}
