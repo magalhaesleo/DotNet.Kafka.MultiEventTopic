6181f4dff6dfd189d831b3c8ea9b9ea62b5f06fb
magalhaesleo <leonardomagalhaes_leo@hotmail.com>
2026-06-06 17:07:50 -0300

    Add root union schema and update schema registration script

--- register-schemas.sh
+++ register-schemas.sh
@@ -21,3 +21,40 @@ register_schema "/schemas/MoneyWithdrawn.avsc" "multi-event-topic-record-name-st
 
 # register EmbeddedUnionType schema
 register_schema "/schemas/BankAccountEvent.avsc" "multi-event-topic-embedded-union-type-value"
+
+# root union schemas
+register_schema "/schemas/AccountBlocked.avsc" "DotNet.Kafka.MultiEventTopic.AccountBlocked"
+register_schema "/schemas/AccountOpened.avsc" "DotNet.Kafka.MultiEventTopic.AccountOpened"
+register_schema "/schemas/MoneyDeposited.avsc" "DotNet.Kafka.MultiEventTopic.MoneyDeposited"
+register_schema "/schemas/MoneyWithdrawn.avsc" "DotNet.Kafka.MultiEventTopic.MoneyWithdrawn"
+jq -n \
+  --rawfile schema /schemas/RootUnion.avsc \
+  '{
+    schema: $schema,
+    references: [
+      {
+        "name":"DotNet.Kafka.MultiEventTopic.AccountBlocked",
+        "subject":"DotNet.Kafka.MultiEventTopic.AccountBlocked",
+        "version":1
+      },
+      {
+        "name":"DotNet.Kafka.MultiEventTopic.AccountOpened",
+        "subject":"DotNet.Kafka.MultiEventTopic.AccountOpened",
+        "version":1
+      },
+      {
+        "name":"DotNet.Kafka.MultiEventTopic.MoneyDeposited",
+        "subject":"DotNet.Kafka.MultiEventTopic.MoneyDeposited",
+        "version":1
+      },
+      {
+        "name":"DotNet.Kafka.MultiEventTopic.MoneyWithdrawn",
+        "subject":"DotNet.Kafka.MultiEventTopic.MoneyWithdrawn",
+        "version":1
+      }
+    ]
+  }' |
+curl -s -X POST -w "%{url}\n" \
+  $SCHEMA_URL/subjects/bank-events-root-union-schema-value/versions \
+  -H "Content-Type: application/vnd.schemaregistry.v1+json" \
+  --data @-
